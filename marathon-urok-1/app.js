2ab2ec2d624444c6d44323e4210013075013f180
Vika Mikhlai <vikamikhlai@gmail.com>
2022-12-08 15:22:14 -0500

    add feat in js

--- marathon-urok-1/app.js
+++ marathon-urok-1/app.js
@@ -1,10 +1,15 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide) {
+    const slides = document.querySelectorAll('.slide');
 
-slides.forEach(slide => {
-    
-    slide.addEventListener('click', () => {
-        slides.forEach(slide => {slide.classList.remove('active');})
-        slide.classList.add('active');
-    })
-});
+    slides[activeSlide].classList.add('active');
 
+    slides.forEach(slide => {
+        
+        slide.addEventListener('click', () => {
+            slides.forEach(slide => {slide.classList.remove('active');})
+            slide.classList.add('active');
+        })
+    });
+}
+
+slidesPlugin(Math.floor(Math.random() * 5));
